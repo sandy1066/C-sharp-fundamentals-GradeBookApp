147a131b0c544b1b2c98f5787597322c17349cc8
sandy1066 <techbrain.sk.1996@gmail.com>
2021-06-19 18:16:55 +0530

    main

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBook book = new DiskBook("Sandeep's Grade Book");
+            book.GradeAdded += OnGradeAdded;
+
+            EnterGrades(book);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average score is {stats.Average:N2}");
+            Console.WriteLine($"Your Grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the grade of 'q' to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("***");
+                }
+
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
+        }
+    }
+}
---
+++ test/GradeBook.Tests/TypeTests.cs
@@ -0,0 +1,145 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public delegate string WriteLogDelegate(string logMessage);
+
+    public class TypeTests
+    {
+
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            var book1 = GetBook("Book1");
+            var book2 = GetBook("Book2");
+
+            Assert.Equal("Book1", book1.Name);
+            Assert.Equal("Book2", book2.Name);
+            Assert.NotSame(book1, book2);
+        }
+
+        [Fact]
+        public void TwoVarialesReferenceSameObject()
+        {
+            var book1 = GetBook("Book1");
+            var book2 = book1;
+
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
+        }
+
+        InMemoryBook GetBook(string name)
+        {
+            return new InMemoryBook(name);
+        }
+
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void SetName(InMemoryBook book, string name)
+        {
+            book.Name = name;
+        }
+
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book1", book1.Name);
+        }
+
+        private void GetBookSetName(InMemoryBook book, string name)
+        {
+            book = new InMemoryBook(name);
+        }
+
+
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book1");
+            GetBookSetName(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetName(ref InMemoryBook book, string name)
+        {
+            book = new InMemoryBook(name);
+        }
+
+
+        [Fact]
+        public void ValueTypeAlsoPassByValue()
+        {
+            var x = GetInt();
+            SetInt(ref x);
+
+            Assert.Equal(42, x);
+        }
+
+        private void SetInt(ref int x)
+        {
+            x = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Sandeep";
+            var upper = MakeUpperCase(name);
+
+            Assert.Equal("Sandeep", name);
+            Assert.Equal("SANDEEP", upper);
+        }
+
+        private string MakeUpperCase(string name)
+        {
+            return name.ToUpper();
+        }
+
+
+        int count = 0;
+
+        [Fact]
+        public void WriteLogDelegateCanPointToMethod()
+        {
+            WriteLogDelegate log = ReturnMessage;
+            log += ReturnMessage;
+            log += IncrementCount;
+
+            var result = log("Hello!");
+            //Assert.Equal("Hello!", result);
+            Assert.Equal(3, count);
+        }
+
+        string ReturnMessage(string message)
+        {
+            count++;
+            return message;
+        }
+
+        string IncrementCount(string message)
+        {
+            count++;
+            return message.ToLower();
+        }
+    }
+}
